c10b9cdde6cba70ca515afae20c7e00772a67fcc
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-04-20 06:29:56 -0500

    progress

--- src/index.tsx
+++ src/index.tsx
@@ -19,38 +19,52 @@ app.use("/*", serveStatic({ root: "./public" }));
 app.get("/new-stack", (c: Context) => {
   return c.html(
     <>
-      <button autofocus onclick="parentElement.close()">
-        Close
-      </button>
       <form hx-post="/stack">
         <div>
           <label for="name">New stack name:</label>
           <input type="text" id="name" name="name" />
         </div>
         <div>
-          <input type="checkbox" name="cpBg" />
-          <label for="name">Copy current background</label>
+          <input type="checkbox" id="copyBg" name="copyBg" />
+          <label for="copyBg">Copy current background</label>
         </div>
         <div>
-          <input type="checkbox" name="openNew" />
-          <label for="name">Open stack in new window</label>
+          <input type="checkbox" id="openNew" name="openNew" />
+          <label for="openNew">Open stack in new window</label>
         </div>
         <div>
           <label for="cardSize">Card size:</label>
           <select id="cardSize" name="cardSize">
-            <option value="small">Small</option>
-            <option value="classic">Classic</option>
-            <option value="powerbook">PowerBook</option>
-            <option value="large">Large</option>
-            <option value="macpaint">MacPaint</option>
-            <option value="window">Window</option>
+            <option>Small</option>
+            <option>Classic</option>
+            <option>PowerBook</option>
+            <option>Large</option>
+            <option>MacPaint</option>
+            <option>Window</option>
           </select>
         </div>
-        <button>Cancel</button>
+        <button autofocus onclick="parentElement.parentElement.close()">
+          Cancel
+        </button>
         <button type="submit">Save</button>
       </form>
     </>
   );
+});
+
+app.post("/stack", async (c: Context) => {
+  const formData = await c.req.formData();
+  const name = formData.get("name");
+  const copyBg = Boolean(formData.get("copyBg"));
+  const openNew = Boolean(formData.get("openNew"));
+  const cardSize = formData.get("cardSize");
+
+  console.log("index.tsx post /stack: name =", name);
+  console.log("index.tsx post /stack: copyBg =", copyBg);
+  console.log("index.tsx post /stack: openNew =", openNew);
+  console.log("index.tsx post /stack: cardSize =", cardSize);
+
+  return c.body(null, 204); // No Content
 });
 
 export default app;
