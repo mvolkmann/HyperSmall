c00bf8fa2b3fb3967f67b326743a0ac561e4029b
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-04-21 13:13:35 -0500

    progress

--- schema.sql
+++ schema.sql
@@ -1,59 +1,72 @@
-create table stacks(
+drop table if exists stacks;
+create table stacks (
   id integer primary key autoincrement,
   cardSize numeric,
   copyBg numeric, -- Boolean
   name string,
   openNew numeric, -- Boolean
-  script string,
+  script string
 );
 
-create table buttons {
+drop table if exists cards;
+create table cards (
   id integer primary key autoincrement,
-  autoHilite numeric, -- Boolean
-  contents string, -- only used when style is Popup
-  enabled numeric, -- Boolean
-  family numeric, -- only used when style is RadioButton
-  iconId numeric,
-  showName numeric, -- Boolean
-  style integer,
-  foreign key (cardId) references cards (id),
-  foreign key (iconId) references icons (id)
-}
-
-create table cards(
-  id integer primary key autoincrement,
-  height: numeric,
+  backgroundId integer, -- foreign key
+  height numeric,
   isBackground numeric, -- boolean
   name string,
   script string,
-  width: numeric,
-  foreign key (backgroundId) references card (id) -- only used when background is false
+  stackId integer, -- foreign key
+  textStyleId integer, -- foreign key
+  width numeric,
+  foreign key (backgroundId) references cards (id) -- only used when background is false
   foreign key (stackId) references stacks (id)
   foreign key (textStyleId) references textStyles (id)
 );
 
-create table fields(
+drop table if exists textStyles;
+create table textStyles (
   id integer primary key autoincrement,
-  autoSelect numeric, -- boolean
-  autoTab numeric, -- boolean
-  doNotSearch numeric, -- boolean
-  doNotWrap numeric, -- boolean
-  height: numeric,
-  name string,
-  script string,
-  width: numeric,
-  foreign key (backgroundId) references card (id) -- only used when background is false
-  foreign key (stackId) references stacks (id)
-  foreign key (textStyleId) references textStyles (id)
-);
-
-create table TextStyle {
-  id integer primary key autoincrement,
-  align: string, -- left, center, or right
+  align string, -- left, center, or right
   bold numeric, -- Boolean
   fontFamily string,
   fontSize integer,
   italic numeric, -- Boolean
   outline numeric, -- Boolean
   shadow numeric, -- Boolean
-  underline numeric, -- Boolean
+  underline numeric -- Boolean
+);
+
+drop table if exists buttons;
+create table buttons (
+  id integer primary key autoincrement,
+  autoHilite numeric, -- Boolean
+  cardId integer, -- foreign key
+  contents string, -- only used when style is Popup
+  enabled numeric, -- Boolean
+  family numeric, -- only used when style is RadioButton
+  iconId integer, -- foreign key
+  showName numeric, -- Boolean
+  style integer,
+  foreign key (cardId) references cards (id),
+  foreign key (iconId) references icons (id)
+);
+
+drop table if exists fields;
+create table fields (
+  id integer primary key autoincrement,
+  autoSelect numeric, -- boolean
+  autoTab numeric, -- boolean
+  backgroundId integer, -- foreign key
+  doNotSearch numeric, -- boolean
+  doNotWrap numeric, -- boolean
+  height numeric,
+  name string,
+  script string,
+  stackId integer, -- foreign key
+  textStyleId integer, -- foreign key
+  width numeric,
+  foreign key (backgroundId) references cards (id) -- only used when background is false
+  foreign key (stackId) references stacks (id)
+  foreign key (textStyleId) references textStyles (id)
+);
